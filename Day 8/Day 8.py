2160a6d29c795bfaa6e4c0a2905b967652773e7b
Sooraj Sannabhadti <You'veGotToMakeLemonade@users.noreply.github.com>
2020-12-10 16:37:47 +0800

    Day 8 Completed!

--- Day 8/Day 8.py
+++ Day 8/Day 8.py
@@ -17,21 +17,40 @@ with open("C:/Users/2005s/Documents/Visual Studio Code/Advent-of-Code-2020/Day 8
         all_lines.append(line)
 print(all_lines)
 
-finished = False
-while finished == False:
-    finished_lines.append(line_no)
-    current_line = all_lines[line_no]
-    current_line = current_line.split(" ")
-    current_command = current_line[0]
-    current_number = int(current_line[1])
-    if "nop" in current_command:
-        line_no = line_no + 1
-    elif "acc" in current_command:
-        accumulator = accumulator + current_number
-        line_no = line_no + 1
-    elif "jmp" in current_command:
-        line_no = line_no + current_number
-    if line_no in finished_lines:
-        finished = True
+changing = 1
+while line_no < len(all_lines):
+    changed = 0
+    line_no = 0
+    finished = False
+    accumulator = 0
+    finished_lines = []
+    while finished == False:
+        finished_lines.append(line_no)
+        current_line = all_lines[line_no]
+        current_line = current_line.split(" ")
+        current_command = current_line[0]
+        current_number = int(current_line[1])
+        if "nop" in current_command:
+            line_no = line_no + 1
+            if changed == changing:
+                line_no = line_no - 1
+                line_no = line_no + current_number
+                print(f"Changed line {line_no} from nop to jmp")
+        elif "acc" in current_command:
+            accumulator = accumulator + current_number
+            line_no = line_no + 1
+        elif "jmp" in current_command:
+            line_no = line_no + current_number
+            if changed == changing:
+                line_no = line_no - current_number
+                line_no = line_no + 1
+                print(f"Changed line {line_no} from jmp to nop")
+        if line_no in finished_lines:
+            finished = True
+        if line_no >= len(all_lines):
+            finished = True
+        changed = changed + 1
+        print(line_no)
+    changing = changing + 1
 
-print(f"The accumulator accumulated a total of {accumulator}")
+print(f"The accumulator accumulated a total of {accumulator}, and the line it was on was {line_no}")
