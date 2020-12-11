3dc38ecc906ebf4477f4bed9ce7dd65ee0cd433e
Sooraj Sannabhadti <2005sooraj@gmail.com>
2020-12-11 13:20:48 +0800

    Update Day 11.py

---
+++ Day 11/Day 11.py
@@ -0,0 +1,12 @@
+"""
+Advent of Code: Day 11
+"""
+
+all_lines = []
+
+with open("C:/Users/2005s/Documents/Visual Studio Code/Advent-of-Code-2020/Day 3/Day 3 Resources.txt", "r") as f:
+    content = f.read()
+    lines = content.splitlines()
+    for line in lines:
+        all_lines.append(line)
+print(all_lines)
